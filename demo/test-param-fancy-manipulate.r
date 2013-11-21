cda28410234f861e5c2d63db371e03ae792bd4c1
Sean Anderson <github@seananderson.ca>
2013-11-20 18:50:45 -0800

    Add in progress testing code for new paradox_pe_sim function

--- demo/test-param-fancy-manipulate.r
+++ demo/test-param-fancy-manipulate.r
@@ -1,16 +1,24 @@
+setwd("~/src/paradox/")
+load_all()
 
-d <- data.frame(expand.grid(alpha = 0.5, beta = 1/150, sigma = seq(0, 0.3, 0.05), num_pop = seq(1, 20), m = 0.1, q = 0.01, cpar = 2, p = 1, n = 1))
+# test
+paradox_pe_sim(t_end = 1000, alpha = rep(0.5, 10), beta = 1/150, m =
+  0.1, q = 0.01, n = 1, num_pop = 10, cpar = 2, p = 1, sigma = 0.2,
+  return_ts = TRUE)
 
-repititions <- 80
+d <- data.frame(expand.grid(alpha = 0.5, beta = 1/150, sigma = seq(0,
+      0.3, 0.05), num_pop = seq(1, 20), m = 0.1, q = 0.01, cpar = 2, p
+    = 1, n = 1))
+
+repititions <- 10
+
 
 out <- plyr::adply(d, 1, function(x) {
-  junk <- plyr::rdply(repititions, function(y) {
-    temp <- with(x, paradox_pe_sim(t_end = 1000, alpha = rep(alpha,
-          num_pop), beta = beta, m = m, q = q, n = n, num_pop =
-        num_pop, cpar = cpar, p = p, sigma = sigma))
-    data.frame(pe = temp$pe, vuln = temp$vuln)
-})
-  data.frame(pe = mean(junk$pe), vuln = mean(junk$vuln))
+  plyr::rdply(repititions, function(y) {
+    with(x, paradox_pe_sim(t_end = 1000, alpha = rep(alpha, num_pop),
+        beta = beta, m = m, q = q, n = n, num_pop = num_pop, cpar =
+        cpar, p = p, sigma = sigma))
+    })
 })
 
 col <- RColorBrewer::brewer.pal(9, "YlOrRd")
