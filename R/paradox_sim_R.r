3dc0845af86b2ba20c44f3cccede96b16738d5b4
Sean Anderson <github@seananderson.ca>
2013-11-21 14:24:21 -0800

    Fix paradox_sim_R to match C++ version

--- R/paradox_sim_R.r
+++ R/paradox_sim_R.r
@@ -10,18 +10,19 @@ paradox_sim_R <- function(num_pop = 5, t_end = 1000, alpha =
   ts <- matrix(0,num_pop+1,t_end)
   ts[,1] <- c(rep(50,num_pop),10)
 
-  stoch_m <- matrix(data = rnorm(num_pop*t_end,0,sigma),num_pop,t_end)
+  stoch_m <- matrix(data = exp(rnorm(num_pop*t_end,-(sigma^2)/2,sigma)),num_pop,t_end)
 
   for (t in 2:t_end) {
     # Dynamics of Fish biomass
     for (i in 1:num_pop) {
       # Shepherd
-      ts[i,t] <- (ts[i,t-1]*(alpha[i] + stoch_m[i,t-1]))/(1 +
-        beta*ts[i,t-1]^n) + ts[i,t-1]*exp(-m-q*ts[Eff,t-1])
+      ts[i,t] <- (ts[i,t-1]*(alpha[i] * stoch_m[i,t-1]))/
+      (1 + beta*ts[i,t-1]^(1/n)) + ts[i,t-1] * exp(-m-q*ts[Eff,t-1])
     }
     # Dynamics of Effort
-    ts[Eff,t] <- ts[Eff,t-1]*exp(-cpar*ts[Eff,t-1]) + p *
-    sum(ts[1:num_pop,t-1]) * (1-exp(-q*ts[Eff,t-1]))
+    total_biomass <- sum(ts[1:num_pop,t-1])
+    ts[Eff,t] <- ts[Eff,t-1]*exp(-cpar) + p * total_biomass *
+    ((q * ts[Eff, t-1])/(m + q * ts[Eff, t-1])) * (1-exp(-m-q*ts[Eff,t-1]))
   }
   return(ts)
 }
