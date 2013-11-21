de422274d3179ba05c5ed5113c20e0db27c0089d
Sean Anderson <github@seananderson.ca>
2013-11-20 19:21:50 -0800

    Large restructuring of package; see details
    I have remove paradox_pe_sim.cpp and get_pe.cpp since these are
    now replaced by the one function that does both.
    
    I have also renamed paradox_pe_sim to paradox_sim to be a little
    less verbose.
    
    I have added documentation and an example to paradox_sim and
    re-run the documenation making for all files.

--- src/paradox_sim.cpp
+++ src/paradox_sim.cpp
@@ -12,26 +12,42 @@ using namespace Rcpp;
 //' @param beta beta parameter in the Shepherd stock-recruit function
 //' (capacity parameter)
 //' @param m Natural mortality
-//' @param n Depensation parameter in the Shepherd stock-recruit
-//' function
+//' @param n n parameter in the Shepherd stock-recruit
+//' function. Controls compensation level.
 //' @param sigma Standard deviation on the simulated alpha values
 //' @param q Effectiveness parameter in the effort function
 //' @param cpar Operational costs parameter in the effort function
 //' @param p Intrinsic value of the fishery parameter in the effort function
+//' @param effort_init Initial effort.
 //' @param biomas_init Initial population biomass. Recycled across
 //' all populations.
-//' @param effort_init Initial effort.
+//' @param vuln_threshold The vulnerability threshold. A proportion (between 0
+//' and 1) indicating the fraction of the mean biomass of a subpopulation before
+//' that subpopulation is declared "vulnerable".
+//' @param burnin The number of years to discard as burnin.
+//' @param return_ts Logical indicating whether the time series should be
+//' returned as part of the output. \code{FALSE} by default to save memory if
+//' running many repetitions.
 //' @author Original model developed by Justin Yeakel. C++ version
-//' implemented by Sean Anderson.
-//' @return A matrix. Time is incremented along the columns and
-//' populations down the rows. The last row represents the effort.
-//'
+//' originally ported by Sean Anderson.
+//' @return A list object. \code{$performance} contains the performance
+//' attributes. If \code{return_ts = TRUE}: \code{$biomass} contains the biomass
+//' matrix (time is incremented along the columns and populations down the rows
+//' without the burnin period removed); \code{$effort} contains the effort in a
+//' numeric vector.
 //' @examples
-//' d <- paradox_sim(alpha = rep(0.5, 10))
-//' matplot(t(d[-11, -c(1:100)]), type = "l", lty = 1)
+//' out <- paradox_sim(alpha = rep(0.5, 10), return_ts = TRUE)
+//' names(out)
+//' print(out$performance)
+//' op <- par(mfrow = c(2, 1), mar = c(4, 4, .5, .5))
+//' matplot(t(out$biomass[, -c(1:500)]), type = "l", lty = 1, xlab = "Year",
+//'   ylab = "Biomass")
+//' plot(out$effort[-c(1:500)], type = "l", ylab = "Effort", xlab = "Year")
+//' par(op)
 //' @export
 // [[Rcpp::export]]
-NumericMatrix paradox_sim(
+
+Rcpp::List paradox_sim(
     int t_end = 1000, 
     int num_pop = 10, 
     NumericVector alpha = NumericVector::create(), 
@@ -43,12 +59,23 @@ NumericMatrix paradox_sim(
     double cpar = 1.4, 
     double p = 0.5,
     double effort_init = 10,
-    double biomass_init = 50
+    double biomass_init = 50,
+    double vuln_thresh = 0.1,
+    int burnin = 500,
+    bool return_ts = false
     ) {
   NumericMatrix biomass(num_pop, t_end);
   NumericVector effort(t_end);
   NumericMatrix ts(num_pop + 1, t_end);
   NumericMatrix stoch_m(num_pop, t_end);
+
+  NumericVector sd_ts(num_pop);
+  NumericVector mean_ts(num_pop);
+  NumericVector total(t_end - burnin);
+  NumericVector CV_ts(num_pop);
+
+  Rcpp::DataFrame performance;
+  Rcpp::List out;
 
   // initialize simulation values in first year
   for (int i = 0; i < num_pop; ++i) {
@@ -71,16 +98,77 @@ NumericMatrix paradox_sim(
         exp(-m-q * effort(j-1));
     }
     double total_biomass = sum(biomass(_, j-1));
-    //effort(j) = effort(j-1) * exp(-cpar * effort(j-1)) + p * total_biomass * (1-exp(-q*effort(j-1)));
     effort(j) = effort(j-1) * exp(-cpar) + p * total_biomass * 
       ((q * effort(j-1))/(m + q * effort(j-1))) * (1-exp(-m-q*effort(j-1)));
   }
-
-  // combine biomass and effort into one matrix
-    for(int i = 0; i < num_pop; ++i) {
-       ts(i, _) = biomass(i, _);
+    
+  // remove burn-in period:
+  NumericMatrix biomass_burned(num_pop, t_end - burnin);
+  for (int j = 0; j < (t_end - burnin); ++j) { // cycle over time
+    for (int i = 0; i < num_pop; ++i) { // cycle over populations
+      biomass_burned(i, j) = biomass(i, j + burnin); 
     }
-    ts(num_pop + 1 - 1, _) = effort;
+  }
 
-  return ts;
+  for (int i = 0; i < num_pop; ++i) {
+    sd_ts(i) = sd(biomass_burned(i,_));
+    mean_ts(i) = mean(biomass_burned(i,_));
+  }
+
+  double mean_sd_ts = mean(sd_ts);
+  double mean_mean_ts = mean(mean_ts);
+
+  for (int j = 0; j < (t_end - burnin); ++j) {
+    total(j) = sum(biomass_burned(_,j));
+  }
+  
+  double sd_total = sd(total);
+  double mean_total = mean(total);
+
+	CV_ts = sd_ts / mean_ts;
+	double CV_total = sd_total / mean_total;
+
+  double PE = mean(CV_ts / CV_total);
+
+  double sync = pow(sd(total), 2)/ pow(sum(sd_ts), 2);
+  
+  // Calculate Vulnerability Metric
+  int vuln_cnt = 0;
+  for (int i = 0; i < num_pop; ++i) {
+    int vuln_TF = 0;
+    for (int j = 0; j < (t_end - burnin); ++j) {
+      if (biomass_burned(i,j) < vuln_thresh * mean_ts(i)) {
+        vuln_TF += 1;
+        }
+    }
+    if (vuln_TF > 0) {
+      vuln_cnt += 1;
+    }
+  }
+  double vuln = vuln_cnt / num_pop;
+  
+  //return Rcpp::List::create(Rcpp::Named("pe") = PE,
+                            //Rcpp::Named("mean_sd_ts") = mean_sd_ts,
+                            //Rcpp::Named("mean_mean_ts") = mean_mean_ts,
+                            //Rcpp::Named("sd_total") = sd_total,
+                            //Rcpp::Named("mean_total") = sd_total,
+                            //Rcpp::Named("sync") = sync,
+                            //Rcpp::Named("vuln") = vuln);
+  performance = Rcpp::DataFrame::create(Rcpp::Named("pe") = PE,
+                Rcpp::Named("mean_sd_ts") = mean_sd_ts,
+                Rcpp::Named("mean_mean_ts") = mean_mean_ts,
+                Rcpp::Named("sd_total") = sd_total,
+                Rcpp::Named("mean_total") = sd_total,
+                Rcpp::Named("sync") = sync,
+                Rcpp::Named("vuln") = vuln);
+  
+  if (return_ts == true) {
+  out = Rcpp::List::create(Rcpp::Named("biomass") = biomass,
+                           Rcpp::Named("effort") = effort,
+                           Rcpp::Named("performance") = performance);
+  } else {
+  out = Rcpp::List::create(Rcpp::Named("performance") = performance);
+  }
+
+  return out;
 }
